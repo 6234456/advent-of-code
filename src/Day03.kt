6141e2730ff40cce67531160ff115401c2175f82
6234456 <6234456@sina.com>
2024-12-03 07:28:36 +0100

    Day03 Reg Java has no g-flag & MatchResult -> groups / range / groupValues

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,22 +1,58 @@
+import java.util.PriorityQueue
 import kotlin.math.abs
 
 fun main() {
     val day = 3
 
-    fun part1(input: List<String>): Int {
-        val arr = input.map { it.trim().split(" ").filter { x -> x.isNotBlank() }.map { x -> x.toInt() } }
-        return arr.size
+    val reg= """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
+    val reg_d = """do\(\)""".toRegex()
+    val reg_dnt = """don't\(\)""".toRegex()
+
+
+    fun part1(input: List<String>): Long {
+        return input.sumOf {
+           s ->
+            reg.findAll(s).fold(0L){ acc: Long, matchResult: MatchResult ->
+                acc + matchResult.groupValues.drop(1).map { it.toLong() }.reduce { acc1, acc2 -> acc1 * acc2 }
+            }
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        val inputString = input.reduce { acc, s ->  acc + s }
+        var flag = true
+
+        val flags =  ArrayDeque<Pair<Int, Boolean>>().apply {
+            this.addAll(
+                (reg_d.findAll(inputString).map {
+                        matchResult ->
+                    Pair(matchResult.groups[0]!!.range.first, true)
+                } + (
+                        reg_dnt.findAll(inputString).map {
+                                matchResult ->
+                            Pair(matchResult.groups[0]!!.range.first, false)
+                        })).sortedBy { it.first }
+            )
+        }
+
+        return reg.findAll(inputString).sumOf{
+            val index = it.range.first
+            while (flags.isNotEmpty() && flags.first().first < index){
+                flag = flags.removeFirst().second
+            }
+
+            if (flag){
+                return@sumOf it.groupValues[2].toLong() * it.groupValues[1].toLong()
+            }
+            return@sumOf 0L
+        }
     }
 
     val testInput1 = readInput("Test${"%02d".format(day)}")
-    check(part1(testInput1) == 2)
+    check(part1(testInput1) == 161L)
 
     val testInput2 = readInput("Test${"%02d".format(day)}")
-    check(part2(testInput2) == 4)
+    check(part2(testInput2) == 48L)
 
     val input = readInput("Data${"%02d".format(day)}")
     part1(input).println()
